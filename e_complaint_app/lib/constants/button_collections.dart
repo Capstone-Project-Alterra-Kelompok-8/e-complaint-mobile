28d9eb1e5a515ba03418a0c12f1b6b078de518c6
fraffi <fraffi43@gmail.com>
2024-05-25 06:01:24 +0700

    initial constants

---
+++ e_complaint_app/lib/constants/button_collections.dart
@@ -0,0 +1,24 @@
+part of 'constants.dart';
+
+class ButtonCollections {
+  static ElevatedButton loginButton = ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: ColorCollections.buttonColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+    ),
+    onPressed: () {},
+    child: const Padding(
+      padding: EdgeInsets.all(PaddingCollections.paddingButtonLarge),
+      child: Text(
+        'Login',
+        style: TextStyle(
+          color: ColorCollections.textSecondaryColor,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ),
+  );
+}
