7e7a81f54c6ab54172680fea1fcaaa974abf10ee
hldaabyu28 <danirahmadani91@gmail.com>
2024-06-02 20:56:43 +0700

    🔨 refactor: update app bar

--- e_complaint_app/lib/views/components/app_bar.dart
+++ e_complaint_app/lib/views/components/app_bar.dart
@@ -44,3 +44,49 @@ class CurvedAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
+
+
+
+class CurvedAppBarMyComplaint extends StatelessWidget implements PreferredSizeWidget {
+  const CurvedAppBarMyComplaint({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ClipRRect(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(48.0),
+            bottomRight: Radius.circular(48.0),
+          ),
+          child: Container(
+            height: 106,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [ColorCollections.primaryColor, ColorCollections.backgroundColor],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: const Image(
+              image: AssetImage('assets/images/logo appbar.png'),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 40,
+          left: 0,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back_outlined),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
