8390d7ed5251aa3bc204ddc38c1e5bdc2c8d4dc9
ecode17 <ecode17@icloud.com>
2024-06-16 02:29:00 +0700

    update

--- e_complaint_app/lib/main.dart
+++ e_complaint_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:e_complaint_app/app_route.dart';
 import 'package:e_complaint_app/controllers/auth_controller.dart';
 import 'package:e_complaint_app/controllers/form_controller.dart';
+import 'package:e_complaint_app/controllers/riwayat_aduan_controller.dart';
 import 'package:e_complaint_app/controllers/user_controller.dart';
 import 'package:e_complaint_app/controllers/profile_controller.dart';
 import 'package:e_complaint_app/controllers/my_complaint_controller.dart';
@@ -36,16 +37,15 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => MyComplaintController()),
         ChangeNotifierProvider(create: (context) => FormAduanController()),
         ChangeNotifierProvider(create: (context) => NewsCommentController()),
-
-
-
+        ChangeNotifierProvider(create: (context) => RiwayatAduanController()),
       ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           useMaterial3: true,
         ),
-        home: SplashScreen(loggedIn: loggedIn), // Set SplashScreen as the initial screen
+        home: SplashScreen(
+            loggedIn: loggedIn), // Set SplashScreen as the initial screen
         routes: AppRoute.routes,
       ),
     );
